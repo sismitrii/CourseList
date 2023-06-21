47e66a2f2a6aba8ad6e069029ec037b0b5f31db0
Katoru <katoru-sama@hotmail.fr>
2023-06-21 11:00:41 +0200

    Homepage now works with header and footer fragments
    Homepage now works with header and footer fragments

--- src/main/java/fr/eni/javaee/shoppinglist/bll/MainServlet.java
+++ src/main/java/fr/eni/javaee/shoppinglist/bll/MainServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.javaee.shoppinglist.bll;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet("/MainServlet")
+@WebServlet("/Homepage")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +28,8 @@ public class MainServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/home.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
@@ -35,7 +37,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
 	}
 
 }
